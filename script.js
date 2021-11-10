f3f5d231e0adb8e0bc89aa5e9cee1b43be8063bf
Kay-du-Plessis <kayladuplessis147@gmail.com>
2021-11-10 13:57:24 +0200

    Finished, it is  now a working game

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-const compScore_span = document.getElementById("compScore");
-const userScore_span = document.getElementById("userScore");
+
+let gamesPlayed = 0;
+let userScore = 0;
+let computerScore = 0;
 const scoreDiv_div = document.querySelector(".scoreDiv");
-const result_div = document.querySelector(".result");
 const rockButton = document.getElementById("r");
 const paperButton = document.getElementById("p");
 const scissorsButton = document.getElementById("s");
+const compScore_span = document.getElementById("compPoints");
+const userScore_span = document.getElementById("userPoints");
 
 rockButton.addEventListener('click', function() {
-    playRound("rock");
+    (playRound("rock"));
 })
 
 paperButton.addEventListener('click', function() {
@@ -29,29 +32,31 @@ function computerPlay() {
 //computerSelection.
 
 function playRound (userSelect) {
-    const computerSelect= computerPlay();
-    console.log(computerSelect);
+    const computerSelect = computerPlay();
+    const result_div = document.querySelector(".result");
+    const compChoice_p = document.querySelector(".compChoice")
+    compChoice_p.textContent = "Computer chose "  + computerSelect + "!"
 
     if (
         (computerSelect == "rock" && userSelect == "scissors") ||
         (computerSelect == "scissors" && userSelect == "paper") ||
         (computerSelect == "paper" && userSelect == "rock")
         ) {
-            console.log("You lose this round, " + computerSelect + " beats " + userSelect);
-            return result = "Lose";
+            result_div.textContent = "You lose this round, " + computerSelect + " beats " + userSelect;
+            scoreTracker(result = "Lose");
 }
     else if (
         (userSelect == "rock" && computerSelect == "scissors") ||
         (userSelect == "scissors" && computerSelect == "paper") ||
         (userSelect == "paper" && computerSelect == "rock")
         ) {
-            console.log("You win this round, " + userSelect + " beats " + computerSelect);
-            return result = "Win";
+            result_div.textContent = "You win this round, " + userSelect + " beats " + computerSelect;
+            scoreTracker(result = "Win");
     }
 
     else {
-        console.log("Tie!");
-        return result = "Tie"
+        result_div.textContent= "Tie!";
+        scoreTracker(result = "Tie");
     }
 }  
 
@@ -59,42 +64,64 @@ function playRound (userSelect) {
 //into this one.
 
 
-function playGame () {
-    let gamesPlayed = 0;
-        let userScore = 0;
-        let computerScore = 0;
+function scoreTracker () {
     
-    for (let i = 0; i < 5; i++) {
-        
-        let totalScore = playRound(computerPlay(), userSelect());
         if (result == "Win") {
             userScore += 1;
             gamesPlayed +=1;
+            userScore_span.textContent = userScore;
             console.log(userScore, computerScore);
         }
 
         else if (result == "Lose") {
              computerScore += 1;
              gamesPlayed +=1;
-            console.log(userScore, computerScore);
-            }
+             compScore_span.textContent = computerScore;
+             console.log(userScore, computerScore);
+        }
         
         else if (result == "Tie") {
             computerScore += 0;
             userScore += 0;
-            console.log(userScore, computerScore);
+            gamesPlayed +=1;
+            userScore_span.textContent = userScore;
+            compScore_span.textContent = computerScore;
+            console.log(userScore, computerScore)
         }
-    }  
+        game();
+}
+
+function game () {
     
-    if (userScore > computerScore) {
-        console.log("You did it! You win!")
+    const finalResult_div = document.getElementById("finalResult");
+
+    if (gamesPlayed == 5 && userScore > computerScore) {
+        finalResult_div.textContent= "Round: " + gamesPlayed + " You did it! You win!";
+        userScore_span.textContent = 0;
+        compScore_span.textContent = 0;
+        userScore = 0;
+        computerScore = 0;
+        gamesPlayed = 0;
     }
 
-    else if (userScore == computerScore) {
-        console.log("Tie! Try again to win")
+    else if (gamesPlayed == 5 && userScore == computerScore) {
+        finalResult_div.textContent="Round: " + gamesPlayed + " Tie! Try again to win";
+        userScore_span.textContent = 0;
+        compScore_span.textContent = 0;
+        userScore = 0;
+        computerScore = 0;
+        gamesPlayed = 0;
+    }
+
+    else if (gamesPlayed == 5 && userScore < computerScore) {
+        finalResult_div.textContent="Round: " + gamesPlayed + " You lost. Try again.";
+        userScore_span.textContent = 0;
+        compScore_span.textContent = 0;
+        userScore = 0;
+        computerScore = 0;
+        gamesPlayed = 0;
     }
 
     else 
-        console.log("You lost. Try again.")
+        finalResult_div.textContent= "Round: " + gamesPlayed + " Make your move."
 }
-playGame()
